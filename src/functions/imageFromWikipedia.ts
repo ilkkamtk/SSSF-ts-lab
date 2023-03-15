95ee990bfba4f20a108778388df6849366b1fde3
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2023-03-15 11:55:04 +0200

    wikimedia

--- src/functions/imageFromWikipedia.ts
+++ src/functions/imageFromWikipedia.ts
@@ -1,1 +1,15 @@
 // function to get image from wikipedia
+
+export default async (query: string) => {
+  try {
+    const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages&piprop=original&origin=*&titles=${query}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const pages = data.query.pages;
+    const page = Object.values(pages)[0];
+    const image = page.original.source;
+    return image;
+  } catch (error) {
+    return 'https://via.placeholder.com/640x480?text=No+image+found';
+  }
+};
--- src/interfaces/WikiImage.ts
+++ src/interfaces/WikiImage.ts
@@ -1,1 +1,19 @@
 // interface for WikiImage
+export default interface WikiImage {
+  batchcomplete: string;
+  query: {
+    pages: {
+      [key: string]: {
+        pageid: number;
+        ns: number;
+        title: string;
+        original: {
+          source: string;
+          width: number;
+          height: number;
+        };
+        pageimage: string;
+      };
+    };
+  };
+}
